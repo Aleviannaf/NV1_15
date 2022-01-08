633607b445c9a54234c64c82ccc760659ef8361c
Alexandre Viana <alexandrevianna511@gmail.com>
2022-01-08 12:14:54 -0400

    consumindo da api

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nv1_15/controllers/home_controller.dart';
 import 'package:nv1_15/models/post_model.dart';
+import 'package:nv1_15/repositories/home_repository_imp.dart';
 import 'package:nv1_15/repositories/home_repository_mock.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeController _controller = HomeController(HomeReositoryMock());
+  final HomeController _controller = HomeController(HomeRepositoryImp());
 
   @override
   void initState() {
@@ -23,15 +24,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Home"),
+      ),
       body: SafeArea(
           child: ValueListenableBuilder<List<PostModel>>(
               valueListenable: _controller.post,
               builder: (_, list, __) {
-                return ListView.builder(
+                return ListView.separated(
                     itemCount: list.length,
                     itemBuilder: (_, index) {
-                      return Text(list[index].title);
-                    });
+                      return ListTile(
+                        leading: Text(list[index].id.toString()),
+                        title: Text(list[index].title),
+                        trailing: Icon(Icons.arrow_right),
+                      );
+                    },
+                    separatorBuilder: (context, index) => Divider(),
+                    );
               })),
     );
   }
---
+++ lib/repositories/home_repository_imp.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:nv1_15/models/post_model.dart';
+import 'package:nv1_15/repositories/home_repository.dart';
+import 'package:dio/dio.dart';
+
+class HomeRepositoryImp implements HomeRepository {
+  @override
+  Future<List<PostModel>> getList() async {
+    try {
+      var response =
+          await Dio().get('https://jsonplaceholder.typicode.com/posts');
+      return (response.data as List).map((e) => PostModel.fromJson(e)).toList();
+    } catch (e) {
+      print(e);
+    }
+    return [];
+  }
+}
